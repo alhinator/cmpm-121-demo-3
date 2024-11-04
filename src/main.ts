57c7321e54dd3a284046e308d6d4b463a1a10894
alhinator <alex@leghart.org>
2024-11-04 09:52:39 -0800

    L16 dialog

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-// todo
+const app = document.querySelector<HTMLDivElement>("#app")!;
+//example starter code 11/4
+const tempButton = document.createElement("button");
+tempButton.innerText = "click!";
+app.appendChild(tempButton);
+tempButton.onclick = () => {
+  alert("clicka da button");
+};
